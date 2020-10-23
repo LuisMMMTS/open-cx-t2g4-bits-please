4e864f579e2b312e5536201129c5aa1df88ad1b9
BrenoAccioly <breno.accioly91@gmail.com>
2020-10-23 12:17:41 +0100

    Added speech to text dependencies

--- src/lib/transcriber.dart
+++ src/lib/transcriber.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class TranscriberPage extends StatefulWidget {
   TranscriberPage({Key key, this.title}) : super(key: key);
