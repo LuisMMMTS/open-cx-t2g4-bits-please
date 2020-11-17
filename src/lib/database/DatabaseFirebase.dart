badc7b0d00d2565e4944e29e9bebec1a6a60eaac
Miguel Pinto <lmiguelapinto@gmail.com>
2020-11-17 18:41:06 +0000

    fixed speakerName variable name

--- src/lib/database/DatabaseFirebase.dart
+++ src/lib/database/DatabaseFirebase.dart
@@ -1,22 +1,26 @@
 import 'package:com_4_all/database/Database.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-class DatabaseFirebase extends Database{
+class DatabaseFirebase extends Database {
   final databaseReference = FirebaseDatabase.instance.reference();
-  void addToken(String talk_name,String token){
-    databaseReference.child(speaker_name).set({
-      'token': token
-    });
+  void addToken(String talk_name, String token) {
+    databaseReference.child(talk_name).set({'token': token});
     ;
   }
-  Future<String> getToken(String speaker_name) async{
+
+  Future<String> getToken(String talk_name) async {
     String out;
-    out = await databaseReference.child(speaker_name).child("token").once().then((DataSnapshot snapshot) {
+    out = await databaseReference
+        .child(talk_name)
+        .child("token")
+        .once()
+        .then((DataSnapshot snapshot) {
       return snapshot.value;
     });
     return out;
   }
-  void removeToken(String speaker_name){
-    databaseReference.child(speaker_name).remove();
+
+  void removeToken(String talk_name) {
+    databaseReference.child(talk_name).remove();
   }
 }
